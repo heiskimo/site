099cc1dac2ee7ff03dc8cfcfdf47cd665ea96c1e
Antoine <antoine@beekast.com>
2019-03-17 19:20:15 +0100

    mobile navigation

--- site/themes/shop-v2/src/js/index.js
+++ site/themes/shop-v2/src/js/index.js
@@ -16,3 +16,41 @@ Snipcart.subscribe('item.added', function (item) {
         buyButton.innerText = buyButtonDefaultText;
     }
 });
+
+
+
+document.addEventListener('DOMContentLoaded', (/* event */) => {
+	// Listen lang chane
+	const langSelect = document.getElementById('lang');
+	langSelect.addEventListener('change', changeLanguage);
+
+    // Listen mobile menu openning
+    const mobileMenuOpen = document.getElementById('mobile-menu-open');
+    mobileMenuOpen.addEventListener('touchstart', openMenu);
+    mobileMenuOpen.addEventListener('click', openMenu);
+
+    // Listen mobile menu closing
+    const mobileMenuClose = document.getElementById('mobile-menu-close');
+    mobileMenuClose.addEventListener('touchstart', closeMenu);
+    mobileMenuClose.addEventListener('click', closeMenu);
+});
+
+function changeLanguage (event) {
+	console.log('change lang', event.target.value);
+}
+
+function openMenu (e) {
+    e.preventDefault();
+    document.body.classList.add('body--mobile-menu-opened');
+    document.addEventListener('touchmove', preventMobileScroll, { passive: false });
+}
+
+function closeMenu (e) {
+    e.preventDefault();
+    document.body.classList.remove('body--mobile-menu-opened');
+    document.removeEventListener('touchmove', preventMobileScroll, { passive: false });
+}
+
+function preventMobileScroll (e) {
+    e.preventDefault();
+}
